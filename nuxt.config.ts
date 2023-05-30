982973e7e43186978ce9b9431914fe0df1a95e81
Pierre Cavalet <pcavalet@kaliop.com>
2023-05-30 17:11:27 +0200

    update ssg config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  nitro: {
+    prerender: {
+      crawlLinks: false,
+      routes: ["/"].concat(
+        Array.from({ length: 10 }, (_, i) => i + 1).map(
+          (id) => `/pokemon/${id}`
+        )
+      ),
+    },
+  },
+});
